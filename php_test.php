c28bddc1a63564e34be9cc0de6b9cef1eac46224
priyak31 <63193469+priyak31@users.noreply.github.com>
2020-12-03 00:07:47 +0530

    Update php_test.php

--- php_test.php
+++ php_test.php
@@ -18,6 +18,51 @@ Ans =>
 
 function get_no_of_server($server_load, $no_of_server, $interval) {  
   for ($i = 1; $i <= $interval; $i++) {
+     if ($i == 5) {
+       if ($server_load[$i] < 50) {
+          $no_of_server_running = $no_of_server/2;
+        }
+       else {
+          $no_of_server_running = 2 * $no_of_server + 1;
+       }
+     }
+  }
+  echo $no_of_server_running." (After 5 minute, ".$no_of_server_running." server is running)";
+}  
+
+$server_load = array("1"=>"10", "2"=>"60", "3"=>"50", "4"=>"15", "5"=>"20",); //server load every minutes
+$no_of_server = 2;
+$interval = count($server_load);
+get_no_of_server($server_load, $no_of_server, $interval);
+
+
+?>
+
+Problem Statement 2:
+Everyday, the restaurant has a limited number of breads, vadas and samosas. The
+restaurant needs to optimise orders in a way that they can generate maximum profit.
+Vadapav needs two bread and a vada
+Samosapav needs two bread and a samosa
+Input:
+First input is: no. of breads, no. of vadas and no. of samosas available in the
+restaurant
+Second input is: Price of Vadapav & price of Samosapav set by the restaurant
+Output:
+Maximum profit possible
+Example:
+Input:
+9 2 3 (9 breads, 2 vada, 3 samosa)
+10 15 (Rs.10 - Vadapav, Rs.15 - Samosapav)
+Output:
+55 (Maximum profit possible is Rs.55)
+
+
+Ans =>
+
+<?php
+  
+ function get_maximum_profit($server_load, $no_of_server, $interval) {  
+  for ($i = 1; $i <= $interval; $i++) {
     if ($server_load[$i] < 50) {
       if ($i == 5) {
         $no_of_server_running = $no_of_server/2;
@@ -32,10 +77,5 @@ function get_no_of_server($server_load, $no_of_server, $interval) {
   echo $no_of_server_running." (After 5 minute, ".$no_of_server_running." server is running)";
 }  
 
-$server_load = array("1"=>"10", "2"=>"60", "3"=>"50", "4"=>"15", "5"=>"20",); //server load every minutes
-$no_of_server = 2;
-$interval = count($server_load);
-get_no_of_server($server_load, $no_of_server, $interval);
 
 
-?>
